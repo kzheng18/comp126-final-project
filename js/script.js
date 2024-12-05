d6b11617dcd43b79c64ab7bfad376ca11fca2e7a
Kevin Zheng <kzheng18@unc.edu>
2024-12-04 21:40:01 -0500

    removed redundant files, added proper href linking without multiple html files, added footer

--- js/script.js
+++ js/script.js
@@ -58,3 +58,17 @@ window.addEventListener("scroll", function () {
     backTopBtn?.classList.remove("active");
   }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").slice(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+      document.body.classList.remove("nav-active");
+    }
+    navbar.classList.remove("active");
+  });
+});
+
